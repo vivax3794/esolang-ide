60f704a7c3e673c3d90979d2b7ce40d0c89d494b
vivax3794 <vivax3794@protonmail.com>
2022-07-06 22:30:11 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,6 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
-});
+  base: process.env.NODE_ENV === "production" ? "/esolang-ide/" : "/",
+}
+);
