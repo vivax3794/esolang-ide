0267d6ad27586a52cc59c8dcde2ad23811212dfe
vivax3794 <vivax3794@protonmail.com>
2022-07-06 22:33:41 +0200

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,5 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
-  base: process.env.NODE_ENV === "production" ? "/esolang-ide/" : "/",
-}
-);
+
+});
